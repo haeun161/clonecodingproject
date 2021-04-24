21b358e9654ded31f45848823285f07c905f8701
이하은 <gracelee161@gmail.com>
2021-04-24 14:44:15 +0900

    header bar; searxh box css

--- amazon.js
+++ amazon.js
@@ -8,9 +8,9 @@ const sideMenuEl = document.getElementById('side__menu')
 
 // 햄버거 바 아이콘에 클릭 이벤트가 발생하면,
 menuOpenEl.addEventListener("click", function() {
-    // 햄버거 바 아이콘을 보이지 않게 처리하고,
+    // 햄버거 바 아이콘을 보이지 않게 처리,
     menuOpenEl.style.display = 'none'
-    // 닫는 아이콘은 보이게 처리합니다
+    // 닫는 아이콘은 보이게 처리
     menuCloseEl.style.display = 'block'
     // 사이드 메뉴를 보이게 합니다 
     sideMenuEl.style.transform = 'translateX(0%)'
@@ -18,9 +18,9 @@ menuOpenEl.addEventListener("click", function() {
 
 // 닫는 아이콘에 클릭 이벤트가 발생하면,
 menuCloseEl.addEventListener("click", function() {
-    // 햄버거 바 아이콘을 보이게 처리하고,
+    // 햄버거 바 아이콘을 보이게 처리,
     menuOpenEl.style.display = 'block'
-    // 닫는 아이콘은 보이지 않게 처리합니다
+    // 닫는 아이콘은 보이지 않게 처리
     menuCloseEl.style.display = 'none'
     // 사이드 메뉴를 원상태로 숨깁니다
     sideMenuEl.style.transform = 'translateX(-100%)'
