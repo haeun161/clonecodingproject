eb58f63f7e04b7b53a530dbb71b0c4ce65696254
이하은 <gracelee161@gmail.com>
2021-05-26 21:28:48 +0900

    fixed a little

--- js/main.js
+++ js/main.js
@@ -31,3 +31,40 @@ menuCloseEl.addEventListener("click", function() {
     // 사이드 메뉴를 원상태로 숨깁니다
     sideMenuEl.style.transform = 'translateX(-100%)'
 })
+
+//슬라이드 시가낯에 따라 자동 넘어가기
+
+
+// 슬라이드 이미지들 선택자
+const $slides = document.querySelectorAll(".slide");
+// 점들 선택자
+const $dots = document.querySelectorAll(".dot");
+
+// 현재 보이는 슬라이드 index값
+let current = 0;
+
+// 자동 슬라이드 기능
+window.addEventListener("load", function () {
+  setInterval(autoSlide, 3000);
+});
+
+const reset = () => {
+  $slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  $dots.forEach((dot) => {
+    dot.style.background = "#F6F5F0";
+    dot.style.width = "8px";
+  });
+};
+
+const autoSlide = () => {
+  reset();
+  if (current === $slides.length - 1) {
+    current = -1;
+  }
+  current++;
+  $slides[current].style.display = "block";
+  $dots[current].style.background = "#1107ff";
+  $dots[current].style.width = "30px";
+};
